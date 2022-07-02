a85eabcd9e345df0c4218b95816d324b07503942
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2022-07-02 20:50:33 -0300

    02/07/2022

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
