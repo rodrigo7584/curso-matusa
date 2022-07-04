69a38e61cf692ab96a98b389b09d90856befbd87
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2022-07-04 17:19:02 -0300

    04/07/2022

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true
+  },
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
