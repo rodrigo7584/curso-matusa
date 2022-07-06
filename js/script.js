ef529b6064440232ed19c3edd332647099f098ca
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2022-07-06 20:44:24 -0300

    06/07/2022

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
-const swiper = new Swiper('.swiper', {
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true
-  },
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
+new Glider(document.querySelector('section.quem-e .glider'), {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: 'section.quem-e .dots',
+  arrows: {
+    prev: 'section.quem-e .glider-prev',
+    next: 'section.quem-e .glider-next'
   }
+})
+new Glider(document.querySelector('section.prova-sucesso .glider'), {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  dots: 'section.prova-sucesso .dots',
+  arrows: {
+    prev: 'section.prova-sucesso .glider-prev',
+    next: 'section.prova-sucesso .glider-next'
+  },
+  responsive: [
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    }
+  ]
 })
